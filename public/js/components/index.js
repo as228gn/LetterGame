a0f8dd869c31aeb05f412b9c78510c883d8ac34b
Anna Ståhlberg <as228gn@student.lnu.se>
2024-05-02 14:41:03 +0200

    the box disapers when moved to the dropzone

--- public/js/components/index.js
+++ public/js/components/index.js
@@ -3,12 +3,12 @@ const dropZone = document.querySelectorAll('.dropZone')
 
 letterDiv.forEach(function (element) {
   element.addEventListener('dragstart', function (event) {
-    event.dataTransfer.setData('text/plain', element.textContent)
-    //event.target.classList.add('dragging')
+    event.dataTransfer.setData('text', event.target.id)
+    //event.target.classList.add('letter')
   })
 
   element.addEventListener('dragend', function (event) {
-  // event.target.classList.add('dragging')
+   //event.target.classList.add('dragging')
   })
 })
 
@@ -19,17 +19,23 @@ dropZone.forEach(function (element) {
   })
 
   element.addEventListener('drop', function (event) {
+console.log(event)
     // Förhindra standardbeteendet
     event.preventDefault()
 
+    // const letter = document.querySelectorAll('.letter')
+    // console.log(letter)
+    // letter.remove()
+
     // Hämta data från drag-and-drop händelse
-    const data = event.dataTransfer.getData('text/plain')
-
+    const data = event.dataTransfer.getData('text')
+    console.log(data)
+    event.target.appendChild(document.getElementById(data))
     // Skapa ett nytt element för att innehålla den släppta bokstaven
-    const droppedLetter = document.createElement('div')
-    droppedLetter.textContent = data
+    // const droppedLetter = document.createElement('div')
+    // droppedLetter.textContent = data
 
-    // Lägg till den släppta bokstaven i dropzonen
-    element.appendChild(droppedLetter)
+    // // Lägg till den släppta bokstaven i dropzonen
+    // element.appendChild(droppedLetter)
   })
 })
